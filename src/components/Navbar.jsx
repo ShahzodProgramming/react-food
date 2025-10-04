3da3daf91eba943001d4d1fd4d84f1aef4039061
ShahzodProgramming <bebap81246@gmail.com>
2025-10-04 11:51:02 +0500

    Back button at navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='w-full bg-amber-300 p-5'>
-        <Link to={'/'} className='text-white text-3xl font-bold'>React shop</Link>
+    <div className="w-full bg-amber-300 p-5 flex justify-between items-center">
+      <Link to={"/"} className="text-white text-3xl font-bold">
+        React shop
+      </Link>
+      <Link to={`/`}>
+        <button className="bg-white rounded p-2 hover:bg-yellow-500 hover:text-white transition">
+          Go back
+        </button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
